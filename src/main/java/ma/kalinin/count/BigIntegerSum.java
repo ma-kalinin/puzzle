e40a3bedfe0b5005a86540f0215f442c1adfb2d6
ma-kalinin <ma-kalinin@protonmail.com>
2024-03-17 23:15:00 +0400

    refactor https://www.codewars.com/kata/525f4206b73515bffb000b21

--- src/main/java/ma/kalinin/count/BigIntegerSum.java
+++ src/main/java/ma/kalinin/count/BigIntegerSum.java
@@ -2,32 +2,36 @@ package ma.kalinin.count;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
 /**
  * Sum two large non-negative integers (possibly with leading zeroes) represented as strings
  * <a href="https://www.codewars.com/kata/525f4206b73515bffb000b21">more details</a>
  */
 @UtilityClass
 public class BigIntegerSum {
-    public static String add(String a, String b) {
-        var first = a.replaceAll("^0*", "");
-        var second = b.replaceAll("^0*", "");
+    public static String add(String first_, String second_) {
+        UnaryOperator<String> stripLeadingZeros = s -> s.replaceAll("^0*", "");
+        UnaryOperator<String> reverse = s -> (new StringBuilder(s)).reverse().toString();
 
-        var smaller = (first.length() > second.length()) ? second : first;
-        var bigger = (smaller.equals(first)) ? second : first;
-        var paddedSmaller = "0".repeat(bigger.length() - smaller.length()) + smaller;
+        var first = stripLeadingZeros.andThen(reverse).apply(first_);
+        var second = stripLeadingZeros.andThen(reverse).apply(second_);
+
+        var result = new StringBuilder();
 
         var carryOver = 0;
-        var result = new StringBuilder();
-        for (int i = bigger.length() - 1; i >= 0; i--) {
-            var sum = intAt(bigger, i) + intAt(paddedSmaller, i) + carryOver;
+        Predicate<Integer> inEither = index -> index < first.length() || index < second.length();
+        for (int i = 0; inEither.test(i) || carryOver > 0; i++) {
+            var sum = digitAtOrZero(first, i) + digitAtOrZero(second, i) + carryOver;
             result.append(sum % 10);
             carryOver = sum / 10;
         }
-        if (carryOver > 0) result.append(carryOver);
+
         return result.reverse().toString();
     }
 
-    private static int intAt(String string, int index) {
-        return Character.digit(string.charAt(index), 10);
+    private static int digitAtOrZero(String string, int index) {
+        return (index >= string.length()) ? 0 : Character.digit(string.charAt(index), 10);
     }
 }
