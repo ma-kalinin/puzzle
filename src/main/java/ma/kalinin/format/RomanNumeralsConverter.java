1065b61a0806a3795ec8d588735151c48318f2d7
ma-kalinin <ma-kalinin@protonmail.com>
2024-05-01 20:53:35 +0400

    refactor arabic to roman numerals converter
    All basic Roman numbers (I, II, III, IV, V etc.) are now statically generated.
    That improves performance and can be used for conversion from Roman to Arabic numerals.

--- src/main/java/ma/kalinin/format/RomanNumeralsConverter.java
+++ src/main/java/ma/kalinin/format/RomanNumeralsConverter.java
@@ -1,28 +1,51 @@
 package ma.kalinin.format;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.toUnmodifiableMap;
 
 public class RomanNumeralsConverter {
 
     private record Digit(Integer arabic, String roman) {}
-    private static final List<Digit> numbers = List.of(
-            new Digit(1000, "M"),
-            new Digit(900, "CM"),
-            new Digit(500, "D"),
-            new Digit(400, "CD"),
-            new Digit(100, "C"),
-            new Digit(90, "XC"),
-            new Digit(50, "L"),
-            new Digit(40, "XL"),
-            new Digit(10, "X"),
-            new Digit(9, "IX"),
-            new Digit(5, "V"),
-            new Digit(4, "IV"),
-            new Digit(1, "I")
-    );
+    private static final List<Digit> numbers;
+    static {    // build all basic numbers that are necessary to represent an arbitrary number
+        var base = List.of(
+                new Digit(1, "I"),
+                new Digit(5, "V"),
+                new Digit(10, "X"),
+                new Digit(50, "L"),
+                new Digit(100, "C"),
+                new Digit(500, "D"),
+                new Digit(1000, "M")
+        );
+
+        var result = new ArrayList<>(base);
+        result.add(new Digit(4, "IV"));
+        result.add(new Digit(9, "IX"));
+        result.add(new Digit(40, "XL"));
+        result.add(new Digit(90, "XC"));
+        result.add(new Digit(400, "CD"));
+        result.add(new Digit(900, "CM"));
+
+        for (int i = 0; i < base.size(); i++) {    // build numbers like VI, VII, VIII etc
+            var isEven = i % 2 == 0;
+            Digit current = base.get(i);
+
+            var roman = isEven ? "" : current.roman;
+            var arabic = isEven ? 0 : current.arabic;
+            Digit toAppend = isEven ? current : base.get(i - 1);
+
+            for (int k = 0; k < 3; k++) {
+                roman += toAppend.roman;
+                arabic += toAppend.arabic;
+                result.add(new Digit(arabic, roman));
+            }
+        }
+
+        numbers = result.stream().distinct().sorted(comparingInt(Digit::arabic).reversed()).toList();
+    }
+
     private static final Map<Integer, String> arabicToRoman = numbers.stream().collect(toUnmodifiableMap(
             Digit::arabic, Digit::roman));
 
@@ -33,22 +56,7 @@ public class RomanNumeralsConverter {
         for (int order = 10; remainder > 0; order *= 10) {
             int part = remainder % order;
             remainder -= part;
-
-            if (arabicToRoman.containsKey(part)) result.insert(0, arabicToRoman.get(part));
-            else result.insert(0, buildRoman(part));
-        }
-
-        return result.toString();
-    }
-
-    private static String buildRoman(Integer part) {
-        var result = new StringBuilder(4);
-
-        for (var digit : numbers) {
-            while (part / digit.arabic > 0) {
-                result.append(digit.roman);
-                part -= digit.arabic;
-            }
+            result.insert(0, arabicToRoman.getOrDefault(part, ""));
         }
 
         return result.toString();
