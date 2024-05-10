8daaad56b3b209756e04e5021965875cf7cb6e66
ma-kalinin <ma-kalinin@protonmail.com>
2024-05-10 14:00:39 +0400

    add test for roman to arabic numerals converter

--- src/test/java/ma/kalinin/format/RomanNumeralsConverterTest.java
+++ src/test/java/ma/kalinin/format/RomanNumeralsConverterTest.java
@@ -9,23 +9,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsConverterTest {
 
-    private record Expectation(Integer arabic, String roman) {}
-    private static Stream<Expectation> expectations() {
+    private record ArabicToRoman(Integer arabic, String roman) {}
+    private static Stream<ArabicToRoman> toRomanExpectations() {
         return Stream.of(
-                new Expectation(1, "I"),
-                new Expectation(11, "XI"),
-                new Expectation(86, "LXXXVI"),
-                new Expectation(1990, "MCMXC"),
-                new Expectation(2008, "MMVIII"),
-                new Expectation(1666, "MDCLXVI"),
-                new Expectation(3999, "MMMCMXCIX")
+                new ArabicToRoman(1, "I"),
+                new ArabicToRoman(11, "XI"),
+                new ArabicToRoman(14, "XIV"),
+                new ArabicToRoman(86, "LXXXVI"),
+                new ArabicToRoman(1990, "MCMXC"),
+                new ArabicToRoman(2008, "MMVIII"),
+                new ArabicToRoman(1666, "MDCLXVI"),
+                new ArabicToRoman(3999, "MMMCMXCIX")
         );
     }
 
+    private record RomanToArabic(String roman, Integer arabic) {}
+    private static Stream<RomanToArabic> toArabicExpectations() {
+        return toRomanExpectations().map(e -> new RomanToArabic(e.roman, e.arabic));
+    }
+
     @ParameterizedTest
-    @MethodSource("expectations")
-    void normalCase(Expectation expectation) {
+    @MethodSource("toRomanExpectations")
+    void toRoman(ArabicToRoman expectation) {
         String actual = RomanNumeralsConverter.toRoman(expectation.arabic);
         assertEquals(expectation.roman, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("toArabicExpectations")
+    void toRoman(RomanToArabic expectation) {
+        Integer actual = RomanNumeralsConverter.toArabic(expectation.roman);
+        assertEquals(expectation.arabic, actual);
     }
 }
